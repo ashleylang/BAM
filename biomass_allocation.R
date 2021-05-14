353f376fc2b1275b59f8b4715c9c3ee821842920
Ashley Lang <ashley504@gmail.com>
2021-05-14 10:09:17 -0400

    Changed a bunch of stuff

--- biomass_allocation.R
+++ biomass_allocation.R
@@ -14,6 +14,12 @@ library(cowplot)
 library(sp)
 library(raster)
 library(corrplot)
+library(lme4)
+library(lmerTest)
+library(sjPlot)
+library(ggeffects)
+library(arm)
+library(MuMIn)
 theme_set(theme_cowplot())
 
 ###get FUNGALROOT data----
@@ -82,7 +88,7 @@ sum <- baad_df %>%
 
 ###MAT/MAP----
 ###Need worldclim data because most of the BAAD database don't have MAT/MAP
-r <- getData("worldclim",var="bio",res=10)
+r <- raster::getData("worldclim",var="bio",res=10)
 r <- r[[c(1,12)]]
 names(r) <- c("Temp","Prec")
 
@@ -103,7 +109,11 @@ df <- cbind.data.frame(coordinates(points),values)%>%
 full_df = baad_df %>% 
   left_join(df, by=c("latitude", "longitude")) %>%
   filter(RmTm>0, h.t >.5, myc_group != "ECM/AM") %>% 
-  unite(study_species, studyName, genus, species, sep="_", remove=F)
+  unite(study_species, studyName, genus, species, sep="_", remove=F) %>% 
+  mutate(log_ht = log(h.t),
+         log_LMRM = log(LMRM),
+         leaf_habit= case_when(pft=="EA" | pft== "EG" ~ "evergreen",
+                               pft=="DA" ~ "deciduous") )
 
 #check distributions
 hist(full_df$LmTm)
@@ -133,8 +143,6 @@ geom_point(aes(color = myc_group), size=3, alpha=0.5)+
 
 #Any strong correlations between continuous variables?
 full_df_cor=full_df %>% 
-  mutate(log_ht = log(h.t),
-         log_LMRM = log(LMRM))%>%
   dplyr::select(Temp, Prec, LmTm, RmTm,  log_LMRM, log_ht) %>% 
   drop_na()
 
@@ -148,37 +156,53 @@ corrplot::corrplot(cor(full_df_cor), method="number", type="upper")
 #temperature not interactive
 #standardize the continuous variables
 #use ggeffects to pull out effect of myc type
-library(lme4)
-library(lmerTest)
-library(sjPlot)
-library(ggeffects)
-library(cowplot)
-library(arm)
-library(MuMIn)
 
 ##root mass/total mass models
-R_EA_m1 <-lmer(RmTm~log(h.t) + myc_group + Temp + (1|study_species), data = subset(full_df, pft == "EA"))
+#Evergreen Angiosperms
+R_EA_m1 <-lmer(RmTm~log_ht + myc_group + Temp + (1|study_species), data = subset(full_df, pft == "EA"))
 summary(R_EA_m1)
 r.squaredGLMM(R_EA_m1)
 tab_model(R_EA_m1, show.se = TRUE, show.ci = FALSE, digits = 3, digits.re = 3, show.std = "std2")
+
 #use ggpredict to plot just the effects of myc group (basically removing height)
 plot(ggpredict(R_EA_m1, terms = c("Temp", "myc_group")))
 ggplot(data = subset(full_df, pft == "EA"), aes(x = Temp, y = RmTm)) + geom_point(aes(color = myc_group))
+
+#use ggpredict to plot just the effects of myc group (basically removing temp)
+plot(ggpredict(R_EA_m1, terms = c("log_ht", "myc_group")))
+ggplot(data = subset(full_df, pft == "EA"), aes(x = log_ht, y = RmTm)) + geom_point(aes(color = myc_group))
+
 #use ggpredict to plot just the effects of myc group (remove Temp and height)
 plot(ggpredict(R_EA_m1, terms = c("myc_group")))
 ggplot(data = subset(full_df, pft == "EA"), aes(x = myc_group, y = RmTm)) + geom_boxplot(aes(color = myc_group))
 
+
+#Deciduous Angiosperms
 R_DA_m1 <-lmer(RmTm~log(h.t) + myc_group + Temp + (1|study_species), data = subset(full_df, pft == "DA"))
 summary(R_DA_m1)
 r.squaredGLMM(R_DA_m1)
 tab_model(R_DA_m1, show.se = TRUE, show.ci = FALSE, digits = 3, digits.re = 3, show.std = "std2")
-plot(ggpredict(R_DA_m1, terms = c("Temp", "myc_group")))
 
+#use ggpredict to plot just the effects of myc group (basically removing height)
+plot(ggpredict(R_DA_m1, terms = c("Temp", "myc_group")))
+ggplot(data = subset(full_df, pft == "DA"), aes(x = Temp, y = RmTm)) + geom_point(aes(color = myc_group))
+
+#use ggpredict to plot just the effects of myc group (remove Temp and height)
+plot(ggpredict(R_DA_m1, terms = c("myc_group")))
+
+#Evergreen Gymnosperms
 R_EG_m1 <-lmer(RmTm~log(h.t) + myc_group + Temp + (1|study_species), data = subset(full_df, pft == "EG"))
 summary(R_EG_m1)
 r.squaredGLMM(R_EG_m1)
 tab_model(R_EG_m1, show.se = TRUE, show.ci = FALSE, digits = 3, digits.re = 3, show.std = "std2")
+
+#use ggpredict to plot just the effects of myc group (basically removing height)
 plot(ggpredict(R_EG_m1, terms = c("Temp", "myc_group")))
+ggplot(data = subset(full_df, pft == "EG"), aes(x = Temp, y = RmTm)) + geom_point(aes(color = myc_group))
+
+#use ggpredict to plot just the effects of myc group (remove Temp and height)
+plot(ggpredict(R_EG_m1, terms = c("myc_group")))
+
 
 ##leaf mass/total mass models
 L_EA_m1 <-lmer(LmTm~log(h.t) + myc_group + Temp + (1|study_species), data = subset(full_df, pft == "EA"))
