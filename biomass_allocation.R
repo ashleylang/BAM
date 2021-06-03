1324e3f2de3cdd3ce413ece9913198127535c8c5
fjevon <fiona.v.jevon.gr@dartmouth.edu>
2021-06-03 15:42:34 -0400

    commit

--- biomass_allocation.R
+++ biomass_allocation.R
@@ -182,7 +182,7 @@ tab_model(R_full_model, show.se = TRUE, show.ci = FALSE, digits = 3, digits.re =
 R_reduced_m1 <-lmer(RmTm~log_ht*leaf_habit + myc_group + Temp + Prec  + (1|study_species), data = full_df)
 vif(R_reduced_m1)
 summary(R_reduced_m1)
-tab_model(R_reduced_m1, show.se = TRUE, show.ci = FALSE, digits = 3, digits.re = 3, show.std = "std2")
+tab_model(R_reduced_m1, show.se = TRUE, show.ci = FALSE, show.std = "std2", digits = 3, digits.re = 3)
 
 #use ggeffect to calculate the marginal effects of myc group and height 
 R_E1 <- ggeffect(R_reduced_m1, terms = c("log_ht[-.7:3.5]", "myc_group"), type = "random")
